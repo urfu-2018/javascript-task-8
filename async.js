6eee89d206f39499d16680481e82943626256c2f
Eien_g <GSKryachko@gmail.com>
2018-11-28 11:34:46 +0500

    Stylefix

--- async.js
+++ async.js
@@ -9,15 +9,14 @@ const isStar = false;
 /** Функция паралелльно запускает указанное число промисов
  * @param {Function<Promise>[]} jobs – функции, которые возвращают промисы
  * @param {Number} parallelNum - число одновременно исполняющихся промисов
- * @param {Number} timeout - таймаут работы промиса
  * @returns {Promise<Array>}
  */
-function runParallel(jobs, parallelNum, timeout = 1000) {
+function runParallel(jobs, parallelNum) {
     if (jobs.length === 0) {
         return Promise.resolve([]);
     }
 
-    return jobs.reduce((p, f) => p.then(f), Promise.resolve())
+    return jobs.reduce((p, f) => p.then(f), Promise.resolve());
 
 }
 
