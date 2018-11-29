d570cbdd7cce1eb7c007943386fd8a4570518cd2
Solodovnikova Ekaterina <colo18@yandex.ru>
2018-11-29 18:33:36 +0500

    yos

--- async.js
+++ async.js
@@ -12,10 +12,47 @@ const isStar = true;
  * @param {Number} timeout - таймаут работы промиса
  * @returns {Promise<Array>}
  */
-function runParallel(jobs, parallelNum, timeout = 1000) {
-    // асинхронная магия
+function runParallel(jobs, parallelNum = jobs.length, timeout = 1000) {
+    const a = splitJobs(jobs, parallelNum);
+    var j = [];
+    a.forEach(function (i) {
+        i.forEach(function (b) {
+            j.push(promiseTimeout(timeout, b.call()));
+        });
+    });
+
+    return Promise.all(j);
 }
 
+function promiseTimeout(ms, promise) {
+    let timeout = new Promise((resolve, reject) => {
+        let id = setTimeout(() => {
+            clearTimeout(id);
+            reject();
+        }, ms);
+    }).catch(() => new Error('Promise timeout'));
+
+    return Promise.race([
+        promise,
+        timeout
+    ]).then(response => response, error => error);
+}
+
+function splitJobs(jobs, parallelNum) {
+    var parallels = [];
+    var i = 0;
+    var start = i;
+    var step = jobs.length / parallelNum;
+    var finish = step;
+    while (i < parallelNum) {
+        parallels.push(jobs.slice(start, finish));
+        start = finish;
+        finish = finish + step;
+        i++;
+    }
+
+    return parallels;
+}
 module.exports = {
     runParallel,
 
