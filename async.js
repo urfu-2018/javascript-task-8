2708525624123d1a67f4aed2d83851e7a7c66088
Barabanshchikova Anastasya <nastya27788@gmail.com>
2018-11-29 11:51:51 +0500

    Решение задачи Мальчишник у Аркадия (пока без допа)

--- async.js
+++ async.js
@@ -4,7 +4,7 @@
  * Сделано задание на звездочку
  * Реализована остановка промиса по таймауту
  */
-const isStar = true;
+const isStar = false;
 
 /** Функция паралелльно запускает указанное число промисов
  * @param {Function<Promise>[]} jobs – функции, которые возвращают промисы
@@ -12,8 +12,23 @@ const isStar = true;
  * @param {Number} timeout - таймаут работы промиса
  * @returns {Promise<Array>}
  */
-function runParallel(jobs, parallelNum, timeout = 1000) {
+function runParallel(jobs, parallelNum) {
     // асинхронная магия
+
+    let jobsArray = [];
+    let index = 0;
+    for (let i = 0; i < jobs.length; i += parallelNum) {
+        jobsArray[index] = jobs.slice(i, i + parallelNum);
+        index++;
+    }
+
+    return Promise.all(jobsArray.map(jobsPacket =>
+        Promise.all(jobsPacket.map(job => nonFailPromise(job())))))
+        .then(results => [].concat(...results));
+}
+
+function nonFailPromise(promise) {
+    return new Promise((resolve) => promise.then(resolve, resolve));
 }
 
 module.exports = {
