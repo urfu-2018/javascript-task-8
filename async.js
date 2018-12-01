c85d965829cd29e7d0db89d91440df4ce5721e3e
Татьяна Гладышева <tatiana.gladysheva24@gmail.com>
2018-12-02 02:58:21 +0500

    Тест

--- async.js
+++ async.js
@@ -13,7 +13,45 @@ const isStar = true;
  * @returns {Promise<Array>}
  */
 function runParallel(jobs, parallelNum, timeout = 1000) {
-    // асинхронная магия
+    if (jobs.length === 0) {
+        return Promise.resolve([]);
+    }
+
+    return new Promise(resolve => {
+        const result = new Array(jobs.length);
+        let completedTasks = 0;
+        let runningTasks = 0;
+        for (var i = 0; i < Math.min(parallelNum, jobs.length); i++) {
+            createTask(i);
+        }
+
+        function createTask(num) {
+            runningTasks += 1;
+            const cb = x => {
+                result[num] = x;
+                completedTasks += 1;
+                if (completedTasks !== jobs.length) {
+                    createTask(runningTasks);
+                } else {
+                    return resolve(result);
+                }
+
+            };
+            runWithTimeout(jobs[num]())
+                .then(cb, cb);
+        }
+    });
+
+    function runWithTimeout(job) {
+        return Promise.race([job,
+            new Promise((resolve, reject) => {
+                const id = setTimeout(() => {
+                    clearTimeout(id);
+                    reject(new Error('promise timeout!'));
+                }, timeout);
+            })]);
+    }
+
 }
 
 module.exports = {
