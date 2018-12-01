5b435815fa3dc64fab54d9c93c717550714a23e7
Arseniy Garelyshev <ars.garelyshev@yandex.ru>
2018-12-01 10:29:05 +0500

    passes tests

--- async.js
+++ async.js
@@ -12,8 +12,24 @@ const isStar = true;
  * @param {Number} timeout - таймаут работы промиса
  * @returns {Promise<Array>}
  */
-function runParallel(jobs, parallelNum, timeout = 1000) {
-    // асинхронная магия
+function runParallel(jobs, parallelNum = 1, timeout = 1000) {
+    let parallelJobs = [];
+    while (jobs.length > 0) {
+        parallelJobs = parallelJobs.concat([jobs.splice(0, parallelNum)]);
+    }
+
+    return Promise.all(parallelJobs.map(thread =>
+        Promise.all(thread.map(job => createRace(job(), timeout)))))
+        .then(result => [].concat(...result));
+}
+
+function createRace(promise, timeout) {
+    const timer = new Promise((resolve, reject) => {
+        setTimeout(reject, timeout);
+    }).catch(() => new Error ('timeout'));
+
+    return Promise.race([promise, timer])
+        .then(result => result);
 }
 
 module.exports = {
