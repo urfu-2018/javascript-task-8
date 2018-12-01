37a5cba9a5144f992d609c2bab525a8dbbfb1f43
Kirill Yuldashev <kirill.yuldashev@mail.ru>
2018-12-01 18:13:35 +0500

    First attempt

--- async.js
+++ async.js
@@ -13,7 +13,48 @@ const isStar = true;
  * @returns {Promise<Array>}
  */
 function runParallel(jobs, parallelNum, timeout = 1000) {
-    // асинхронная магия
+    if (jobs.length === 0) {
+        return Promise.resolve(jobs);
+    }
+
+    parallelNum = Math.min(parallelNum, jobs.length);
+
+    return new Promise(function (resolve) {
+        const results = [];
+
+        let currentId = 0;
+
+        function createCb(jobId) {
+            return function (result) {
+                results[jobId] = result;
+
+                if (results.length === jobs.length) {
+                    return resolve(results);
+                }
+
+                if (currentId < jobs.length) {
+                    processJob(currentId++);
+                }
+            };
+        }
+
+        function processJob(jobId) {
+            const promise = jobs[jobId]();
+
+            const timeoutPromise = new Promise((_, reject) =>
+                setTimeout(reject, timeout, new Error('Promise timeout')));
+
+            const cb = createCb(jobId);
+
+            Promise
+                .race([promise, timeoutPromise])
+                .then(cb, cb);
+        }
+
+        for (let id = 0; id < parallelNum; id++) {
+            processJob(currentId++);
+        }
+    });
 }
 
 module.exports = {
