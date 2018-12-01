ab6fe92f88d9cff1108566e93392d358680ecec2
Timofey Utrobin <timofeyutrobin@gmail.com>
2018-12-02 01:32:01 +0500

    done without star

--- async.js
+++ async.js
@@ -4,7 +4,7 @@
  * Сделано задание на звездочку
  * Реализована остановка промиса по таймауту
  */
-const isStar = true;
+const isStar = false;
 
 /** Функция паралелльно запускает указанное число промисов
  * @param {Function<Promise>[]} jobs – функции, которые возвращают промисы
@@ -12,8 +12,26 @@ const isStar = true;
  * @param {Number} timeout - таймаут работы промиса
  * @returns {Promise<Array>}
  */
-function runParallel(jobs, parallelNum, timeout = 1000) {
-    // асинхронная магия
+async function runParallel(jobs, parallelNum/* timeout = 1000 */) {
+    if (jobs.length === 0) {
+        return Promise.reject([]);
+    }
+    const result = [];
+    while (jobs.length !== 0) {
+        const currentJobs = jobs.splice(0, parallelNum);
+        const currentResult = await Promise.all(currentJobs.map(doJob));
+        result.push(...currentResult);
+    }
+
+    return Promise.resolve(result);
+}
+
+async function doJob(job) {
+    try {
+        return job();
+    } catch (error) {
+        return error;
+    }
 }
 
 module.exports = {
