9d7721f00018e3c2a7ec8e8a1b13ee1077e9012b
Daria <DaArtPetrova@mail.ru>
2018-12-02 16:46:22 +0500

    First attempt

--- async.js
+++ async.js
@@ -12,8 +12,46 @@ const isStar = true;
  * @param {Number} timeout - таймаут работы промиса
  * @returns {Promise<Array>}
  */
+
 function runParallel(jobs, parallelNum, timeout = 1000) {
-    // асинхронная магия
+    const result = [];
+    let startedJobsCount = 0;
+    let finishedJobsCount = 0;
+
+    return new Promise(resolve => {
+        if (jobs.length === 0) {
+            resolve(result);
+        } else {
+            while (startedJobsCount < parallelNum && startedJobsCount < jobs.length) {
+                start(startedJobsCount, resolve);
+                startedJobsCount++;
+            }
+        }
+    });
+
+    function start(jobNumber, resolve) {
+        const takeResult = jobResult => getResult(jobNumber, jobResult, resolve);
+        jobProcess(jobNumber).then(takeResult, takeResult);
+    }
+
+    function jobProcess(jobNumber) {
+        return new Promise((resolveJob, rejectJob) => {
+            jobs[jobNumber]().then(resolveJob, rejectJob);
+            setTimeout(() => rejectJob(new Error('Promise timeout')), timeout);
+        });
+    }
+
+    function getResult(jobNumber, jobResult, resolve) {
+        result[jobNumber] = jobResult;
+        finishedJobsCount++;
+        if (finishedJobsCount === jobs.length) {
+            resolve(result);
+        }
+        if (startedJobsCount < jobs.length) {
+            start(startedJobsCount, resolve);
+            startedJobsCount++;
+        }
+    }
 }
 
 module.exports = {
