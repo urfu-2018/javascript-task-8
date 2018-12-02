1145e782f23ae68e1054c8827c4fd9ea6de9abbb
Alex768 <vortigont768@inbox.ru>
2018-12-02 12:54:07 +0500

    initial, without key.json

--- async.js
+++ async.js
@@ -13,7 +13,37 @@ const isStar = true;
  * @returns {Promise<Array>}
  */
 function runParallel(jobs, parallelNum, timeout = 1000) {
-    // асинхронная магия
+    return new Promise(resolve => {
+        if (! (jobs && jobs.length)) {
+            resolve([]);
+        }
+        // счетчик-указатель на индекс не выполненной задачи из jobs
+        let nextNotHandled = 0;
+        const TRANSLATIONS = [];
+        const resolveTranslation = (index) => {
+            // обработчик результата запроса на получение перевода
+            const requestResolver = (result) => {
+                TRANSLATIONS[index] = result;
+                if (TRANSLATIONS.length === jobs.length) {
+                    resolve(TRANSLATIONS);
+                } else {
+                    resolveTranslation(nextNotHandled++);
+                }
+            };
+            // выполнение запроса на получение перевода
+            new Promise((innerResolver) => {
+                setTimeout(innerResolver, timeout, new Error('Promise timeout'));
+                jobs[index]()
+                    .then(innerResolver)
+                    .catch(innerResolver);
+            })
+                .then(requestResolver);
+        };
+        // запуск первых parallelNum промисов
+        for (let i = 0; i < parallelNum; i++) {
+            resolveTranslation(nextNotHandled++);
+        }
+    });
 }
 
 module.exports = {
