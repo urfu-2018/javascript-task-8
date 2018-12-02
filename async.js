13f3fd095318afe8888177a5a0a3f71399002839
Liza Ginzburg <lizagin98@gmail.com>
2018-12-02 15:31:46 +0500

    first

--- async.js
+++ async.js
@@ -13,7 +13,50 @@ const isStar = true;
  * @returns {Promise<Array>}
  */
 function runParallel(jobs, parallelNum, timeout = 1000) {
-    // асинхронная магия
+
+    return new Promise(function (resolve) {
+        if (jobs.length === 0) {
+            resolve(jobs);
+        }
+
+        parallelNum = Math.min(parallelNum, jobs.length);
+
+        const results = [];
+
+        let current = 0;
+
+        const createHandler = id => result => {
+            results[id] = result;
+
+            if (results.length === jobs.length) {
+                return resolve(results);
+            }
+
+            if (current < jobs.length) {
+                handleJob(current++);
+            }
+        };
+
+        async function handleJob(id) {
+            const promise = jobs[id]();
+
+            const timeoutPromise = new Promise((_, reject) =>
+                setTimeout(reject, timeout, new Error('Promise timeout')));
+
+            const handle = createHandler(id);
+
+            try {
+                const result = await Promise.race([promise, timeoutPromise]);
+                handle(result);
+            } catch (error) {
+                handle(error);
+            }
+        }
+
+        for (let id = 0; id < parallelNum; id++) {
+            handleJob(current++);
+        }
+    });
 }
 
 module.exports = {
