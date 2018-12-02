347a2bd13f63e009877405ea947564bc620bb033
OlgaCheraneva <32261747+OlgaCheraneva@users.noreply.github.com>
2018-12-02 17:26:18 +0500

    Update async.js

--- async.js
+++ async.js
@@ -13,7 +13,37 @@ const isStar = true;
  * @returns {Promise<Array>}
  */
 function runParallel(jobs, parallelNum, timeout = 1000) {
-    // асинхронная магия
+    if (!jobs || jobs.length === 0) {
+        return Promise.resolve([]);
+    }
+
+    return new Promise(resolve => {
+        const count = Math.min(jobs.length, parallelNum);
+        let translations = new Array(count);
+        let jobIndex = 0;
+
+        const translate = id => {
+            const addTranslation = translation => {
+                translations[id] = translation;
+                if (translations.length !== jobs.length) {
+                    translate(jobIndex++);
+                } else {
+                    return resolve(translations);
+                }
+            };
+            const promise = new Promise(resolver => {
+                setTimeout(resolver, timeout, new Error('Promise timeout'));
+                jobs[id]().then(resolver)
+                    .catch(resolver);
+            });
+
+            return promise.then(addTranslation);
+        };
+
+        while (jobIndex <= count) {
+            translate(jobIndex++);
+        }
+    });
 }
 
 module.exports = {
