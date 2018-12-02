113220dcf7baa35ec4a4b208ca60f4839c1b9c5e
Yewert <blazer.andrew@gmail.com>
2018-12-02 20:54:51 +0500

    k

--- async.js
+++ async.js
@@ -6,6 +6,59 @@
  */
 const isStar = true;
 
+function limit(timeout) {
+    return new Promise(resolve => setTimeout(resolve, timeout))
+        .then(() => {
+            throw new Error();
+        });
+}
+
+class JobScheduler {
+    constructor(jobs, concurrencyLevel, timeout) {
+        this.jobs = jobs;
+        this.concurrencyLevel = concurrencyLevel;
+        this.timeout = timeout;
+        this.jobIndex = 0;
+        this.results = [];
+    }
+
+    produceWrappedJob(promiseFactory) {
+        return Promise.race([
+            promiseFactory(),
+            limit(this.timeout)
+        ]);
+    }
+
+    putResultAndRunNext(result, index) {
+        this.results[index] = result;
+        if (this.results.length === this.jobs.length) {
+            this.resolve(this.results);
+
+            return;
+        }
+
+        this.runJob();
+    }
+
+    runJob() {
+        const index = this.jobIndex++;
+        const job = this.jobs[index];
+
+        this.produceWrappedJob(job)
+            .then(value => this.putResultAndRunNext(value, index))
+            .catch(error => this.putResultAndRunNext(error, index));
+    }
+
+    run() {
+        return new Promise(resolve => {
+            this.resolve = resolve;
+            for (let i = 0; i < this.concurrencyLevel; i++) {
+                this.runJob();
+            }
+        });
+    }
+}
+
 /** Функция паралелльно запускает указанное число промисов
  * @param {Function<Promise>[]} jobs – функции, которые возвращают промисы
  * @param {Number} parallelNum - число одновременно исполняющихся промисов
@@ -13,7 +66,11 @@ const isStar = true;
  * @returns {Promise<Array>}
  */
 function runParallel(jobs, parallelNum, timeout = 1000) {
-    // асинхронная магия
+    if (jobs.length === 0) {
+        return Promise.resolve([]);
+    }
+
+    return new JobScheduler(jobs, parallelNum, timeout).run();
 }
 
 module.exports = {
