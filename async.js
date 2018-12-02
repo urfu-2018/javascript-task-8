530d2a7ff3ccd155b1a2dead157a428de3390f8e
lenastar <lena.starkova.1998@mail.ru>
2018-12-02 16:58:57 +0300

    Faled

--- async.js
+++ async.js
@@ -13,7 +13,41 @@ const isStar = true;
  * @returns {Promise<Array>}
  */
 function runParallel(jobs, parallelNum, timeout = 1000) {
-    // асинхронная магия
+    if (jobs.length === 0) {
+        return Promise.resolve([]);
+    }
+
+    let results = [];
+    let indexOfJob = 0;
+
+    return new Promise(
+        resolve => {
+            function makeResult(currentIndex, result) {
+                results[currentIndex] = result;
+                if (currentIndex === jobs.length) {
+                    return resolve(results);
+                }
+                if (indexOfJob < jobs.length) {
+                    doJob(indexOfJob++);
+                }
+            }
+
+            function doJob(currentIndex) {
+                let runningJob = jobs[currentIndex]();
+                let timeOutJob = new Promise(reject =>
+                    setTimeout(reject, timeout, new Error('Promise timeout')));
+
+                Promise
+                    .race([runningJob, timeOutJob])
+                    .then(result => makeResult(currentIndex, result))
+                    .catch(error => makeResult(currentIndex, error));
+            }
+
+            while (indexOfJob < parallelNum) {
+                doJob(indexOfJob++);
+            }
+        }
+    );
 }
 
 module.exports = {
