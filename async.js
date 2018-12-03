d8ee5e20a959e19fcd57405c24734933393cf657
kartsev <kartsev@skbkontur.ru>
2018-12-03 19:46:46 +0500

    first try

--- async.js
+++ async.js
@@ -4,16 +4,36 @@
  * Сделано задание на звездочку
  * Реализована остановка промиса по таймауту
  */
-const isStar = true;
+const isStar = false;
 
 /** Функция паралелльно запускает указанное число промисов
  * @param {Function<Promise>[]} jobs – функции, которые возвращают промисы
  * @param {Number} parallelNum - число одновременно исполняющихся промисов
- * @param {Number} timeout - таймаут работы промиса
  * @returns {Promise<Array>}
  */
-function runParallel(jobs, parallelNum, timeout = 1000) {
+function runParallel(jobs, parallelNum) {
+    let promise = new Promise((x) => x([]));
+    if (jobs.length === 0) {
+        return Promise.resolve(promise);
+    }
     // асинхронная магия
+
+    for (let i = 0; i < jobs.length; i += parallelNum) {
+        promise = promise.then(result => {
+            let values = jobs.splice(0, parallelNum);
+            let pr = Promise.all(values.map(x => x()))
+                .then(x => {
+                    result.push(x);
+
+                    return result;
+                });
+
+            return Promise.resolve(pr);
+        });
+
+    }
+
+    return promise;
 }
 
 module.exports = {
