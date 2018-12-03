b5d1d2700fa77f1734022dc0f125f719cf438db7
Dm17r1y <dmitriy.kopylov2016@gmail.com>
2018-12-03 19:21:05 +0500

    start

--- async.js
+++ async.js
@@ -6,18 +6,59 @@
  */
 const isStar = true;
 
+async function workerFunction(jobs, jobTimeout) {
+    const workerResult = [];
+    while (jobs.length > 0) {
+        const job = jobs.pop();
+        const jobIndex = jobs.length;
+        try {
+            const result = await setPromiseTimeout(job(), jobTimeout);
+            workerResult.push([result, jobIndex]);
+        } catch (error) {
+            workerResult.push([error, jobIndex]);
+        }
+    }
+
+    return workerResult;
+}
+
+function setPromiseTimeout(promise, timeout) {
+    return new Promise((resolve, reject) => {
+        const timeoutID = setTimeout(() => reject(new Error('Promise timeout')), timeout);
+        promise
+            .then(result => {
+                clearTimeout(timeoutID);
+                resolve(result);
+            });
+    });
+}
+
 /** Функция паралелльно запускает указанное число промисов
  * @param {Function<Promise>[]} jobs – функции, которые возвращают промисы
  * @param {Number} parallelNum - число одновременно исполняющихся промисов
  * @param {Number} timeout - таймаут работы промиса
  * @returns {Promise<Array>}
  */
-function runParallel(jobs, parallelNum, timeout = 1000) {
-    // асинхронная магия
+async function runParallel(jobs, parallelNum, timeout = 1000) {
+    const totalResult = [];
+    const workers = [];
+    jobs.reverse();
+    for (let i = 0; i < parallelNum; i++) {
+        workers.push(workerFunction(jobs, timeout));
+    }
+
+    const workersResult = await Promise.all(workers);
+    workersResult.forEach(resultArray => {
+        resultArray.forEach(([result, jobIndex]) => {
+            totalResult[jobIndex] = result;
+        });
+    });
+    totalResult.reverse();
+
+    return totalResult;
 }
 
 module.exports = {
     runParallel,
-
     isStar
 };
