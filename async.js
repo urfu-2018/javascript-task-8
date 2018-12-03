34083e3805ff01e72a52d2cf36beaecfc2a0c145
Drusalyator <And-gridin@yandex.ru>
2018-12-03 23:31:44 +0500

    init without extra

--- async.js
+++ async.js
@@ -4,7 +4,7 @@
  * Сделано задание на звездочку
  * Реализована остановка промиса по таймауту
  */
-const isStar = true;
+const isStar = false;
 
 /** Функция паралелльно запускает указанное число промисов
  * @param {Function<Promise>[]} jobs – функции, которые возвращают промисы
@@ -12,8 +12,37 @@ const isStar = true;
  * @param {Number} timeout - таймаут работы промиса
  * @returns {Promise<Array>}
  */
-function runParallel(jobs, parallelNum, timeout = 1000) {
-    // асинхронная магия
+function runParallel(jobs, parallelNum) {
+    return new Promise(resolve => {
+        if (!jobs.length) {
+            return resolve([]);
+        }
+
+        const results = [];
+        let currentJobIndex = 0;
+
+        for (let i = 0; i < Math.min(jobs.length, parallelNum); i++) {
+            doJob(currentJobIndex++);
+        }
+
+        function createCallBack(jobIndex) {
+            return function (result) {
+                results[jobIndex] = result;
+                if (results.length === jobs.length) {
+                    return resolve(result);
+                }
+                if (jobIndex < jobs.length) {
+                    doJob(currentJobIndex++);
+                }
+            };
+        }
+
+        function doJob(jobIndex) {
+            const callBack = createCallBack(jobIndex);
+
+            jobs[jobIndex]().then(callBack, callBack);
+        }
+    });
 }
 
 module.exports = {
