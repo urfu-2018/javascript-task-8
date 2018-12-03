eefe307d75a634195d96d5bbee9562df3f65df65
Egor Naumov <Egor_Nmv@bk.ru>
2018-12-04 00:39:49 +0500

    Base+

--- async.js
+++ async.js
@@ -13,7 +13,36 @@ const isStar = true;
  * @returns {Promise<Array>}
  */
 function runParallel(jobs, parallelNum, timeout = 1000) {
-    // асинхронная магия
+    if (jobs.length === 0) {
+        return Promise.resolve([]);
+    }
+
+    return new Promise(resolve => {
+        const res = [];
+        let curri = 0;
+        const countOfParallel = Math.min(jobs.length, parallelNum);
+        while (curri < countOfParallel) {
+            startNewPromise(curri++);
+        }
+
+        async function startNewPromise(index) {
+            function addJob(job) {
+                res[index] = job;
+                if (jobs.length > res.length) {
+                    startNewPromise(curri++);
+                }
+                if (res.length === jobs.length) {
+                    resolve(res);
+                }
+            }
+
+            return (new Promise(reject => {
+                setTimeout(reject, timeout, new Error('Promise timeout'));
+                jobs[index]().then(reject, reject);
+            }))
+                .then(addJob);
+        }
+    });
 }
 
 module.exports = {
