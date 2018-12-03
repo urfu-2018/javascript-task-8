8e83f71ddeeb79ec614b32d8a7831ed0a162b0bf
artem <artem.gafetinov@gmail.com>
2018-12-03 23:34:09 +0500

    Done!

--- async.js
+++ async.js
@@ -13,7 +13,32 @@ const isStar = true;
  * @returns {Promise<Array>}
  */
 function runParallel(jobs, parallelNum, timeout = 1000) {
-    // асинхронная магия
+    return new Promise(resolve => {
+        const results = new Array(jobs.length);
+        let finished = 0;
+        let started = 0;
+        for (var i = 0; i < Math.min(parallelNum, jobs.length); i++) {
+            run(i);
+        }
+        function run(num) {
+            const callback = result => {
+                results[num] = result;
+                if (++finished !== jobs.length) {
+                    run(++started);
+                } else {
+                    return resolve(results);
+                }
+            };
+            promiseTimeout(jobs[num]())
+                .then(callback, callback);
+        }
+    });
+    function promiseTimeout(job) {
+        return Promise.race([job,
+            new Promise((resolve, reject) => {
+                setTimeout(() => reject(new Error('Promise timeout')), timeout);
+            })]);
+    }
 }
 
 module.exports = {
