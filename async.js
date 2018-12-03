4d371b1a498c59a333a8e7db24b5e929baedac41
hukopo@yandex.ru <opokuH1998>
2018-12-03 18:19:44 +0500

    star ^_^

--- async.js
+++ async.js
@@ -4,7 +4,7 @@
  * Сделано задание на звездочку
  * Реализована остановка промиса по таймауту
  */
-const isStar = false;
+const isStar = true;
 
 /** Функция паралелльно запускает указанное число промисов
  * @param {Function<Promise>[]} jobs – функции, которые возвращают промисы
@@ -12,7 +12,7 @@ const isStar = false;
  * @param {Number} timeout - таймаут работы промиса
  * @returns {Promise<Array>}
  */
-function runParallel(jobs, parallelNum) {
+function runParallel(jobs, parallelNum, timeout = 1000) {
     let results = new Array(jobs.length);
     let count = 0;
     let globalResolve;
@@ -28,8 +28,16 @@ function runParallel(jobs, parallelNum) {
         }
     });
 
+    async function getJobPromise(indexJob) {
+        return new Promise((resolve, reject) => {
+            jobs[indexJob]()
+                .then(resolve);
+            setTimeout(reject, timeout, new Error('Promise timeout'));
+        });
+    }
+
     function runJob(indexJob) {
-        return jobs[indexJob]()
+        return getJobPromise(indexJob)
             .then(result => {
                 results[indexJob] = result;
             })
