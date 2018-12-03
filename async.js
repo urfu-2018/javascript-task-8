cf28e51650b9864bf2bbc40dfabe956903cb5580
Max Ratkov <32338709+mattgroy@users.noreply.github.com>
2018-12-03 20:04:32 +0500

    async magic

--- async.js
+++ async.js
@@ -13,11 +13,49 @@ const isStar = true;
  * @returns {Promise<Array>}
  */
 function runParallel(jobs, parallelNum, timeout = 1000) {
-    // асинхронная магия
+    let results = [];
+
+    return new Promise(
+        resolve => {
+            if (!jobs.length) {
+                resolve(results);
+
+                return;
+            }
+
+            let jobCounter = 0;
+            jobs.slice(0, parallelNum)
+                .forEach(job => start(job, jobCounter++));
+
+
+            function start(job, index) {
+                const handle = result => handleResult(result, index);
+                Promise
+                    .race([job(), new Promise(rejectJob =>
+                        setTimeout(rejectJob, timeout, new Error('Promise timeout')))])
+                    .then(handle)
+                    .catch(handle);
+            }
+
+
+            function handleResult(result, index) {
+                results[index] = result;
+
+                if (Object.keys(results).length === jobs.length) {
+                    resolve(results);
+
+                    return;
+                }
+
+                if (jobCounter < jobs.length) {
+                    start(jobs[jobCounter], jobCounter++);
+                }
+            }
+        });
 }
+
 
 module.exports = {
     runParallel,
-
     isStar
 };
