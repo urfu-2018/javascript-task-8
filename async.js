102f3f036222af8dda2b89c3f089f760b1de685b
Nikita Chikunov <ninjaken32@gmail.com>
2018-12-04 01:34:15 +0500

    init solution

--- async.js
+++ async.js
@@ -4,7 +4,7 @@
  * Сделано задание на звездочку
  * Реализована остановка промиса по таймауту
  */
-const isStar = true;
+const isStar = false;
 
 /** Функция паралелльно запускает указанное число промисов
  * @param {Function<Promise>[]} jobs – функции, которые возвращают промисы
@@ -12,8 +12,39 @@ const isStar = true;
  * @param {Number} timeout - таймаут работы промиса
  * @returns {Promise<Array>}
  */
+// eslint-disable-next-line no-unused-vars
 function runParallel(jobs, parallelNum, timeout = 1000) {
-    // асинхронная магия
+    if (!jobs.length) {
+        return Promise.resolve([]);
+    }
+
+    const resultTranslate = [];
+    let counter = 0;
+    const thrower = error => error;
+
+    function startTranslate(anotherTranslate) {
+        if (!anotherTranslate.length) {
+            return;
+        }
+
+        return anotherTranslate.shift()()
+            .catch(thrower)
+            .then(res => {
+                resultTranslate[counter] = res;
+                counter++;
+
+                startTranslate(anotherTranslate);
+            });
+    }
+
+    const translates = [];
+
+    while (parallelNum >= 0 && jobs.length !== 0) {
+        translates.push(startTranslate(jobs));
+        parallelNum--;
+    }
+
+    return Promise.all(translates).then(() => resultTranslate);
 }
 
 module.exports = {
