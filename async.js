a4b4c2a021a3be45a6b975ff68839784c413be8c
Монин Константин <33116124+Kuvadorian@users.noreply.github.com>
2018-12-03 15:55:25 +0500

    Complete

--- async.js
+++ async.js
@@ -13,7 +13,43 @@ const isStar = true;
  * @returns {Promise<Array>}
  */
 function runParallel(jobs, parallelNum, timeout = 1000) {
-    // асинхронная магия
+    console.info(jobs, parallelNum, timeout);
+
+    return new Promise(resolve => {
+        const result = [];
+        let startedCount = 0;
+
+        if (jobs.length === 0 || parallelNum <= 0) {
+            resolve([]);
+        }
+
+        while (startedCount < parallelNum) {
+            startPromise(startedCount++);
+        }
+
+        function startPromise(jobIndex) {
+            return handler(jobIndex)
+                .then(resolve1 => finishPromise(resolve1, jobIndex));
+        }
+
+        function handler(jobIndex) {
+            return new Promise((resolve2, reject) => {
+                jobs[jobIndex]()
+                    .then(resolve2, reject);
+                setTimeout(reject, timeout, new Error('Promise timeout'));
+            });
+        }
+
+        function finishPromise(res, jobIndex) {
+            result[jobIndex] = res;
+
+            if (startedCount < jobs.length) {
+                startPromise(startedCount++);
+            } else if (jobs.length === result.length) {
+                resolve(result);
+            }
+        }
+    });
 }
 
 module.exports = {
