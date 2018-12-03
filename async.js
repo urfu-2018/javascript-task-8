b1d7e532ad19cffccc0d0dd74a4435d59b7bd181
kartsev <kartsev@skbkontur.ru>
2018-12-04 04:47:34 +0500

    maybe now

--- async.js
+++ async.js
@@ -4,37 +4,41 @@
  * Сделано задание на звездочку
  * Реализована остановка промиса по таймауту
  */
-const isStar = false;
+const isStar = true;
 
 /** Функция паралелльно запускает указанное число промисов
  * @param {Function<Promise>[]} jobs – функции, которые возвращают промисы
  * @param {Number} parallelNum - число одновременно исполняющихся промисов
  * @returns {Promise<Array>}
+ * @param {Number} timeout - таймаут работы промиса
  */
-function runParallel(jobs, parallelNum) {
+function runParallel(jobs, parallelNum, timeout = 1000) {
     const result = [];
-    let currentJobIndex = 0;
-    let finishedJobsCount = 0;
+    let curIndex = 0;
+    let finishedJobs = 0;
     if (!jobs.length || parallelNum === 0) {
         return Promise.resolve(result);
     }
-    return new Promise(resolve => {
+
+    return new Promise(commonResolve => {
 
         const begin = () => {
-            if (finishedJobsCount >= jobs.length) {
-                resolve(result);
+            if (finishedJobs >= jobs.length) {
+                commonResolve(result);
             }
-            if (currentJobIndex < jobs.length) {
-                jobs[currentJobIndex]()
-                    .then(x => {
-                        result[currentJobIndex] = x;
-                    },
-                    x => {
-                        result[currentJobIndex] = x;
-                    })
-                    .then(() => finishedJobsCount++)
+            if (curIndex < jobs.length) {
+                new Promise((resolve, reject) => {
+                    setTimeout(() => reject(new Error('Promise timeout')), timeout);
+                    jobs[curIndex]().then(resolve, reject);
+                }).then(x => {
+                    result[curIndex] = x;
+                },
+                x => {
+                    result[curIndex] = x;
+                })
+                    .then(() => finishedJobs++)
                     .then(begin);
-                currentJobIndex++;
+                curIndex++;
             }
         };
 
