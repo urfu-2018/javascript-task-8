0fa9fa2eb4cb5eaae86d778fa85e6893b1db45e4
Nerzull3 <dima3.98@mail.ru>
2018-12-03 16:03:22 +0500

    1sa attempt

--- async.js
+++ async.js
@@ -13,7 +13,40 @@ const isStar = true;
  * @returns {Promise<Array>}
  */
 function runParallel(jobs, parallelNum, timeout = 1000) {
-    // асинхронная магия
+    return new Promise(resolve => {
+        if (jobs.length === 0) {
+            resolve([]);
+        }
+
+        const doneJobs = [];
+        let currentIndex = 0;
+
+        while (currentIndex < parallelNum) {
+            doJob(currentIndex++);
+        }
+
+        async function doJob(jobIndex) {
+            await executeWithTimeout(doneJobs, jobs[jobIndex], jobIndex, timeout);
+
+            if (doneJobs.length === jobs.length) {
+                resolve(doneJobs);
+            }
+
+            if (currentIndex < jobs.length) {
+                doJob(currentIndex++);
+            }
+        }
+    });
+}
+
+async function executeWithTimeout(doneJobs, currentJob, index, timeout) {
+    const timeoutPromise = new Promise((resolve, reject) =>
+        setTimeout(() => reject(new Error('Promise timeout')), timeout)
+    );
+
+    doneJobs[index] = Promise
+        .race([await currentJob(), timeoutPromise])
+        .then(resolve => resolve, reject => reject);
 }
 
 module.exports = {
