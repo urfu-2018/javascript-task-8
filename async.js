198e35fd528f73ce33d291a62b0529c7de5b8121
Unknown <valera9719@gmail.com>
2018-12-04 00:06:48 +0500

    commit

--- async.js
+++ async.js
@@ -13,7 +13,43 @@ const isStar = true;
  * @returns {Promise<Array>}
  */
 function runParallel(jobs, parallelNum, timeout = 1000) {
-    // асинхронная магия
+    let results = new Array(jobs.length);
+    let startJobs = 0;
+    let finishedJobs = 0;
+
+    return new Promise(resolve => {
+        if (!jobs.length) {
+            resolve(results);
+        }
+        while (startJobs < parallelNum) {
+            runJob(startJobs++, resolve);
+        }
+    });
+
+    function finishJobWithTimeout(jobIndex) {
+        return new Promise((timeResolve, timeReject) => {
+            jobs[jobIndex]()
+                .then(timeResolve, timeReject);
+            setTimeout(timeReject, timeout, new Error('Promise timeout'));
+        });
+    }
+
+    function runJob(indexJob, resolve) {
+        let jobResult = result => finishJob(result, indexJob, resolve);
+
+        return finishJobWithTimeout(indexJob).then(jobResult)
+            .catch(jobResult);
+    }
+
+    function finishJob(result, jobIndex, resolve) {
+        results[jobIndex] = result;
+        finishedJobs++;
+        if (finishedJobs === jobs.length) {
+            resolve(results);
+        } else if (startJobs < jobs.length) {
+            runJob(startJobs++, resolve);
+        }
+    }
 }
 
 module.exports = {
