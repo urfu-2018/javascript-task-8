a870fb32e1eb45008e67cf402be0a46abb589b81
Buka174 <43822018+Buka174@users.noreply.github.com>
2018-12-03 19:16:13 +0500

    Update async.js

--- async.js
+++ async.js
@@ -1,12 +1,12 @@
 'use strict';
 
-/
+/**
  * Сделано задание на звездочку
  * Реализована остановка промиса по таймауту
  */
 const isStar = true;
 
-/ Функция паралелльно запускает указанное число промисов
+/** Функция паралелльно запускает указанное число промисов
  * @param {Function<Promise>[]} jobs – функции, которые возвращают промисы
  * @param {Number} parallelNum - число одновременно исполняющихся промисов
  * @param {Number} timeout - таймаут работы промиса
@@ -16,6 +16,7 @@ function runParallel(jobs, parallelNum = jobs.length, timeout = 1000) {
     return new Promise(resolve => {
         var currentJob = 0;
         var jobsResults = [];
+
         if (!jobs.length) {
             resolve(jobs);
         }
@@ -24,11 +25,13 @@ function runParallel(jobs, parallelNum = jobs.length, timeout = 1000) {
             doJob(jobs[currentJob], currentJob++);
             i++;
         }
+
         function doJob(job, currentJobIndex) {
             const resolveResult = result => finishJob(result, currentJobIndex);
             promiseTimeout(timeout, job())
                 .then(resolveResult, resolveResult);
         }
+
         function finishJob(result, index) {
             jobsResults[index] = result;
             if (jobsResults.length === jobs.length) {
@@ -40,12 +43,14 @@ function runParallel(jobs, parallelNum = jobs.length, timeout = 1000) {
         }
     });
 }
+
 function promiseTimeout(ms, promise) {
-    let timeout = new Promise((_resolve, reject) => {
+    let timeout = new Promise((resolve, reject) => {
         setTimeout(() => {
             reject(new Error('Promise timeout'));
         }, ms);
     });
+
     return Promise.race([
         promise,
         timeout
