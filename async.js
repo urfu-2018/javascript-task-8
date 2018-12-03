4a8dbd5cee352c9688fd17243999b0b30faf1237
Bolkonyashka <33488476+Bolkonyashka@users.noreply.github.com>
2018-12-03 22:43:57 +0500

    magic is here

--- async.js
+++ async.js
@@ -13,7 +13,40 @@ const isStar = true;
  * @returns {Promise<Array>}
  */
 function runParallel(jobs, parallelNum, timeout = 1000) {
-    // асинхронная магия
+    return new Promise(resolve => {
+        if (jobs.length === 0) {
+            resolve([]);
+        }
+
+        let results = [];
+        let jobNumber = 0;
+
+        for (let i = 0; i < Math.min(jobs.length, parallelNum); i++) {
+            runNextJob(jobNumber++);
+        }
+
+        function runNextJob(jobNum) {
+
+            function handleResult(res) {
+                results[jobNum] = res;
+
+                if (results.length === jobs.length) {
+                    return resolve(results);
+                }
+
+                if (jobNum < jobs.length) {
+                    runNextJob(jobNumber++);
+                }
+            }
+
+            Promise.race([
+                jobs[jobNum](),
+                new Promise((_, reject) => {
+                    setTimeout(reject, timeout, new Error('Timeout error'));
+                })
+            ]).then(handleResult, handleResult);
+        }
+    });
 }
 
 module.exports = {
