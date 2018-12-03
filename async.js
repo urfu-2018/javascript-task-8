434877dbf3db4f6c54518bc69d884d86115ea895
AndrewLoginov <andrew.loginov13@gmail.com>
2018-12-04 00:30:01 +0600

    Логинов Андрей
    Решение(1)

--- async.js
+++ async.js
@@ -13,7 +13,34 @@ const isStar = true;
  * @returns {Promise<Array>}
  */
 function runParallel(jobs, parallelNum, timeout = 1000) {
-    // асинхронная магия
+    if (parallelNum <= 0 || !jobs.length) {
+        return Promise.resolve([]);
+    }
+    let results = [];
+    let succeedJobs = 0;
+    function range(restJobs) {
+        if (!restJobs.length) {
+            return;
+        }
+        let i = succeedJobs++;
+        let nextJob = Promise.race([
+            new Promise(resolve => setTimeout(resolve, timeout, new Error('Promise timeout'))),
+            restJobs.shift()().catch(error => error)
+        ]);
+
+        return nextJob.then((result) => {
+            results[i] = result;
+
+            return range(restJobs);
+        });
+    }
+    let ranges = [];
+    while (parallelNum-- > 0 && jobs.length > 0) {
+        ranges.push(range(jobs));
+    }
+
+    return Promise.all(ranges).then(() => results);
+
 }
 
 module.exports = {
