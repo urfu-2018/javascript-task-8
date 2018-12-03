b4b85db947e082836524b8335e50b3844c122b79
evilPaprika <ivil.paprika@gmail.com>
2018-12-03 15:15:20 +0500

    сколько тестов?

--- async.js
+++ async.js
@@ -14,9 +14,12 @@ const isStar = false;
  */
 async function runParallel(jobs, parallelNum) {
     console.info(parallelNum);
+    if (!jobs.length) {
+        return new Promise((resolve) => resolve([]));
+    }
     const result = [];
     for (const job of jobs) {
-        result.push(await (job()));
+        result.push(await job().then(x => x, y => y));
     }
 
     return new Promise((resolve) => resolve(result));
