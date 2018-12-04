125c4a30af9f28d0532839da3730c200f4d1c593
evilPaprika <ivil.paprika@gmail.com>
2018-12-04 12:43:53 +0500

    extra

--- async.js
+++ async.js
@@ -4,7 +4,7 @@
  * Сделано задание на звездочку
  * Реализована остановка промиса по таймауту
  */
-const isStar = false;
+const isStar = true;
 
 /** Функция паралелльно запускает указанное число промисов
  * @param {Function<Promise>[]} jobs – функции, которые возвращают промисы
@@ -12,18 +12,24 @@ const isStar = false;
  * @param {Number} timeout - таймаут работы промиса
  * @returns {Promise<Array>}
  */
-async function runParallel(jobs, parallelNum) {
+async function runParallel(jobs, parallelNum, timeout = 1000) {
     const queue = [];
     const result = [];
     const jobDone = (response, promise, jobIndex) => {
         queue.splice(queue.indexOf(promise), 1);
         result[jobIndex] = response;
     };
+
     for (const [index, job] of jobs.entries()) {
-        const promise = job().then(
-            resolve => jobDone(resolve, promise, index),
-            error => jobDone(error, promise, index)
-        );
+        const promise = Promise.race([
+            job().then(
+                resolve => jobDone(resolve, promise, index),
+                error => jobDone(error, promise, index)
+            ),
+            new Promise(resolve => {
+                setTimeout(resolve, timeout, new Error('Promise timeout'));
+            }).then(error => jobDone(error, promise, index))
+        ]);
         queue.push(promise);
         if (queue.length >= parallelNum) {
             await Promise.race(queue);
