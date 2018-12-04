3052ca4c27753b0b57afb200f1cf3af69cea3ff7
Раджабов Роман <38882414+RadzhabovRoman@users.noreply.github.com>
2018-12-04 13:09:17 +0500

    Добавил решение задачи

--- async.js
+++ async.js
@@ -4,7 +4,7 @@
  * Сделано задание на звездочку
  * Реализована остановка промиса по таймауту
  */
-const isStar = true;
+const isStar = false;
 
 /** Функция паралелльно запускает указанное число промисов
  * @param {Function<Promise>[]} jobs – функции, которые возвращают промисы
@@ -13,7 +13,36 @@ const isStar = true;
  * @returns {Promise<Array>}
  */
 function runParallel(jobs, parallelNum, timeout = 1000) {
-    // асинхронная магия
+    return new Promise(resolve => {
+        if (!jobs.length || parallelNum <= 0) {
+            resolve([]);
+        }
+        let results = [];
+        let startIndex = 0;
+        for (; startIndex < parallelNum; startIndex++) {
+            startJob(startIndex);
+        }
+
+        function startJob(jobIndex) {
+            Promise.race([
+                jobs[jobIndex](),
+                new Promise((rejectTime) => {
+                    setTimeout(rejectTime, timeout, new Error('Promise timeout'));
+                })
+            ])
+                .then(jobResult => finishJob(jobResult, jobIndex));
+        }
+
+        function finishJob(jobResult, jobIndex) {
+            results[jobIndex] = jobResult;
+            if (startIndex < jobs.length) {
+                startJob(startIndex++);
+            }
+            if (results.length === jobs.length) {
+                resolve(results);
+            }
+        }
+    });
 }
 
 module.exports = {
