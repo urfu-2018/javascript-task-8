beb48b8660d632eab6fceb2e2af7618794ad6154
Leofwin <andy_namor@mail.ru>
2018-12-04 14:40:28 +0500

    done task

--- async.js
+++ async.js
@@ -13,7 +13,44 @@ const isStar = true;
  * @returns {Promise<Array>}
  */
 function runParallel(jobs, parallelNum, timeout = 1000) {
-    // асинхронная магия
+    return new Promise(resolve => {
+        if (jobs.length === 0) {
+            resolve([]);
+        }
+
+        const queue = [];
+        for (let i = 0; i < jobs.length; i++) {
+            queue.push(jobs[i]);
+        }
+
+        const result = [];
+        const count = Math.min(jobs.length, parallelNum);
+
+        for (let i = 0; i < count; i++) {
+            doWork();
+        }
+
+        function doWork() {
+            if (queue.length === 0) {
+                resolve(result);
+
+                return;
+            }
+
+            const item = queue.shift();
+            Promise.race([item(), startTimer()])
+                .then(res => result.push(res))
+                .then(doWork, doWork);
+        }
+
+        function startTimer() {
+            return new Promise((then, reject) => {
+                setTimeout(() => {
+                    reject(new Error('Promise timeout'));
+                }, timeout);
+            });
+        }
+    });
 }
 
 module.exports = {
