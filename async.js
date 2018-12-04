0abca94bd06acbaf2716f8c91d4ab5b59733b80c
Leofwin <andy_namor@mail.ru>
2018-12-04 15:02:03 +0500

    make with async

--- async.js
+++ async.js
@@ -12,44 +12,47 @@ const isStar = true;
  * @param {Number} timeout - таймаут работы промиса
  * @returns {Promise<Array>}
  */
-function runParallel(jobs, parallelNum, timeout = 1000) {
-    return new Promise(resolve => {
-        if (jobs.length === 0) {
-            resolve([]);
+async function runParallel(jobs, parallelNum, timeout = 1000) {
+    if (jobs.length === 0) {
+        return [];
+    }
+
+    const queue = [];
+    for (let i = 0; i < jobs.length; i++) {
+        queue.push(jobs[i]);
+    }
+
+    const result = [];
+    const count = Math.min(jobs.length, parallelNum);
+
+    const workers = [];
+    for (let i = 0; i < count; i++) {
+        workers.push(doWork(queue, result, timeout));
+    }
+
+    await Promise.all(workers);
+
+    return result;
+}
+
+async function doWork(queue, result, timeout) {
+    while (queue.length > 0) {
+        const item = queue.shift();
+
+        if (item === undefined) {
+            continue;
         }
 
-        const queue = [];
-        for (let i = 0; i < jobs.length; i++) {
-            queue.push(jobs[i]);
-        }
+        await Promise.race([item(), startTimer(timeout)])
+            .then(res => result.push(res), res => result.push(res));
+    }
+}
 
-        const result = [];
-        const count = Math.min(jobs.length, parallelNum);
-
-        for (let i = 0; i < count; i++) {
-            doWork();
-        }
-
-        function doWork() {
-            if (queue.length === 0) {
-                resolve(result);
-
-                return;
-            }
-
-            const item = queue.shift();
-            Promise.race([item(), startTimer()])
-                .then(res => result.push(res), res => result.push(res))
-                .then(doWork, doWork);
-        }
-
-        function startTimer() {
-            return new Promise((then, reject) => {
-                setTimeout(() => {
-                    reject(new Error('Promise timeout'));
-                }, timeout);
-            });
-        }
+function startTimer(timeout) {
+    return new Promise((then, reject) => {
+        setTimeout(() => {
+            reject(new Error('Promise timeout'));
+        }, timeout);
     });
 }
 
