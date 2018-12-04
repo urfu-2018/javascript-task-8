f0ebe7e34ed2e20059dfe2814e48f158db09a10f
FoOkySNick <FoOkySNick@ya.ru>
2018-12-04 09:58:16 +0500

    Add files via upload

--- async.js
+++ async.js
@@ -13,7 +13,44 @@ const isStar = true;
  * @returns {Promise<Array>}
  */
 function runParallel(jobs, parallelNum, timeout = 1000) {
-    // асинхронная магия
+    return new Promise(function (resolve) {
+        if (jobs.length === 0 || typeof jobs === 'undefined') {
+            return resolve([]);
+        }
+
+        let currentIndex = 0;
+
+        const resultArray = [];
+
+        function react(response, index) {
+            if (currentIndex < jobs.length) {
+                adjustTimeOutAndReact(currentIndex++);
+            }
+
+            resultArray[index] = response;
+
+            if (resultArray.length === jobs.length) {
+                return resolve(resultArray);
+            }
+        }
+        function adjustTimeOutAndReact(index) {
+            const timeOut = new Promise(function (resolve1, reject1) {
+                return setTimeout(reject1, timeout, new Error('Promise timeout'));
+            });
+
+            Promise.race([jobs[index](), timeOut])
+                .then(res => {
+                    react(res, index);
+                })
+                .catch(err => {
+                    react(err, index);
+                });
+        }
+
+        for (let i = 0; i < jobs.length && parallelNum > 0; i++, parallelNum--) {
+            adjustTimeOutAndReact(currentIndex++);
+        }
+    });
 }
 
 module.exports = {
