86877751f2b593bc212dc960d274e5b95074cdb1
zarix908 <daniyarrus@gmail.com>
2018-12-04 06:45:26 -0600

    init

--- async.js
+++ async.js
@@ -5,15 +5,51 @@
  * Реализована остановка промиса по таймауту
  */
 const isStar = true;
+let lastStartedIndex = 0;
+
+function executeJob(jobIndex, resolve, results, jobs) {
+    let job = jobs[jobIndex];
+    job()
+        .then(x => exitJob({ 'result': x, 'index': jobIndex },
+            resolve, results, jobs))
+        .catch(x => exitJob({ 'result': x, 'index': jobIndex },
+            resolve, results, jobs));
+}
+
+function exitJob(resultObject, resolve, results, jobs) {
+    results[resultObject.index] = resultObject.result;
+    if (results.length === jobs.length) {
+        resolve(results);
+
+        return;
+    }
+
+    if (lastStartedIndex < jobs.length) {
+        executeJob(lastStartedIndex++, resolve, results, jobs);
+    }
+}
 
 /** Функция паралелльно запускает указанное число промисов
  * @param {Function<Promise>[]} jobs – функции, которые возвращают промисы
  * @param {Number} parallelNum - число одновременно исполняющихся промисов
- * @param {Number} timeout - таймаут работы промиса
+ *  @param {Number} timeout - таймаут
  * @returns {Promise<Array>}
  */
-function runParallel(jobs, parallelNum, timeout = 1000) {
-    // асинхронная магия
+async function runParallel(jobs, parallelNum) {
+    return new Promise(resolve => {
+        const results = [];
+
+        if (jobs.length === 0) {
+            resolve([]);
+
+            return;
+        }
+
+        while (lastStartedIndex < parallelNum) {
+            executeJob(lastStartedIndex++, resolve, results, jobs);
+        }
+    });
+
 }
 
 module.exports = {
