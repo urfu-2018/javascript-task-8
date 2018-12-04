462e39560706c62b26faa467e660a919e001b3c4
cfgi <cfgi@yandex.ru>
2018-12-04 14:56:34 +0500

    First attempt, without extra

--- async.js
+++ async.js
@@ -4,7 +4,7 @@
  * Сделано задание на звездочку
  * Реализована остановка промиса по таймауту
  */
-const isStar = true;
+const isStar = false;
 
 /** Функция паралелльно запускает указанное число промисов
  * @param {Function<Promise>[]} jobs – функции, которые возвращают промисы
@@ -12,8 +12,38 @@ const isStar = true;
  * @param {Number} timeout - таймаут работы промиса
  * @returns {Promise<Array>}
  */
-function runParallel(jobs, parallelNum, timeout = 1000) {
-    // асинхронная магия
+function runParallel(jobs, parallelNum) {
+
+    if (jobs.length === 0) {
+        Promise.resolve(jobs);
+    }
+
+    let promiseArray = [];
+
+    function startPromise(index, currentJob, resolve) {
+
+        function addPromiseToArray() {
+
+            promiseArray[currentJob - 1] = jobs[currentJob - 1];
+
+            if (jobs.length === promiseArray.length) {
+                resolve(promiseArray);
+            } else {
+                startPromise(currentJob++, currentJob, resolve);
+            }
+        }
+
+        return (new Promise(() => {
+            jobs[currentJob - 1]()
+                .then(addPromiseToArray);
+        }));
+    }
+
+    return new Promise(resolve => {
+        for (let currentJob = 0; currentJob < parallelNum; currentJob++) {
+            startPromise(currentJob++, currentJob, resolve);
+        }
+    });
 }
 
 module.exports = {
