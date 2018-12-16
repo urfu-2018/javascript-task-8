6f5d6b0b7523c5a40d3bd088c0a61ad641192c96
hukopo <hukopo@yandex.ru>
2018-12-16 15:36:59 +0500

    refactor

--- async.js
+++ async.js
@@ -5,6 +5,10 @@
  * Реализована остановка промиса по таймауту
  */
 const isStar = true;
+let results;
+let count = 0;
+let globalResolve;
+let countFinished = 0;
 
 /** Функция паралелльно запускает указанное число промисов
  * @param {Function<Promise>[]} jobs – функции, которые возвращают промисы
@@ -12,11 +16,8 @@ const isStar = true;
  * @param {Number} timeout - таймаут работы промиса
  * @returns {Promise<Array>}
  */
-async function runParallel(jobs, parallelNum, timeout = 1000) {
-    let results = new Array(jobs.length);
-    let count = 0;
-    let globalResolve;
-    let countFinished = 0;
+function runParallel(jobs, parallelNum, timeout = 1000) {
+    results = new Array(jobs.length);
 
     if (jobs.length === 0) {
         return [];
@@ -28,41 +29,40 @@ async function runParallel(jobs, parallelNum, timeout = 1000) {
             runJob(count++);
         }
     });
+}
 
-    async function getJobPromise(indexJob) {
-        return new Promise((resolve, reject) => {
-            jobs[indexJob]()
-                .then(resolve, reject);
-            setTimeout(reject, timeout, new Error('Promise timeout'));
+function getJobPromise(indexJob) {
+    return new Promise((resolve, reject) => {
+        jobs[indexJob]()
+            .then(resolve, reject);
+        setTimeout(reject, timeout, new Error('Promise timeout'));
+    });
+}
+
+function runJob(indexJob) {
+    return getJobPromise(indexJob)
+        .then(result => {
+            results[indexJob] = result;
+            goNextOrQuit();
+        })
+        .catch(result => {
+            results[indexJob] = result;
+            goNextOrQuit();
         });
-    }
+}
 
-    function runJob(indexJob) {
-        return getJobPromise(indexJob)
-            .then(result => {
-                results[indexJob] = result;
-                goNextOrQuit();
-            })
-            .catch(result => {
-                results[indexJob] = result;
-                goNextOrQuit();
-            });
-    }
-
-    function goNextOrQuit() {
-        countFinished++;
-        if (count < jobs.length) {
-            runJob(count++);
-        } else if (countFinished === jobs.length) {
-            end();
-        }
-    }
-
-    function end() {
-        globalResolve(results);
+function goNextOrQuit() {
+    countFinished++;
+    if (count < jobs.length) {
+        runJob(count++);
+    } else if (countFinished === jobs.length) {
+        end();
     }
 }
 
+function end() {
+    globalResolve(results);
+}
 
 module.exports = {
     runParallel,
