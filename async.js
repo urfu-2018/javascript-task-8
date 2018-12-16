2674267f379faf28d179e263ff52950f795cc8ea
epeshk <1197228@gmail.com>
2018-12-16 21:39:31 +0500

    try

--- async.js
+++ async.js
@@ -4,7 +4,7 @@
  * Сделано задание на звездочку
  * Реализована остановка промиса по таймауту
  */
-const isStar = true;
+const isStar = false;
 
 /** Функция паралелльно запускает указанное число промисов
  * @param {Function<Promise>[]} jobs – функции, которые возвращают промисы
@@ -13,32 +13,30 @@ const isStar = true;
  * @returns {Promise<Array>}
  */
 function runParallel(jobs, parallelNum, timeout = 1000) {
-    if (jobs.length === 0) {
-        return new Promise(resolve => resolve([]));
-    }
+    timeout.toString();
 
     return new Promise(resolve => {
+        if (jobs.length === 0) {
+            resolve([]);
+        }
+
         let results = [];
         let index = 0;
-        let running = 0;
 
         function runNext() {
             const current = index++;
 
-            if (running === 0 || current >= jobs.length) {
+            if (current >= jobs.length) {
                 return resolve(results);
             }
 
-            running++;
-
             function continuation(r) {
                 results[current] = r;
-                --running;
                 runNext();
             }
 
-            runWithTimeout(jobs[current], timeout)
-                .then(continuation)
+            jobs[current]()
+                .then(continuation, continuation)
                 .catch(continuation);
         }
 
@@ -48,14 +46,14 @@ function runParallel(jobs, parallelNum, timeout = 1000) {
     });
 }
 
-function runWithTimeout(job, timeout) {
-    return Promise.race([job, timeoutPromise(timeout)]);
-}
+// function runWithTimeout(job, timeout) {
+//     return Promise.race([job, timeoutPromise(timeout)]);
+// }
 
-function timeoutPromise(timeout) {
-    return new Promise(
-        (resolve, reject) => setTimeout(() => reject(new Error('Promise timeout')), timeout));
-}
+// function timeoutPromise(timeout) {
+//     return new Promise(
+//         (resolve, reject) => setTimeout(() => reject(new Error('Promise timeout')), timeout));
+// }
 
 module.exports = {
     runParallel,
