b3d2fc245b1455e7b4b8753f6a540e8f05dda1de
epeshk <1197228@gmail.com>
2018-12-16 19:02:15 +0500

    first try

--- async.js
+++ async.js
@@ -13,7 +13,41 @@ const isStar = true;
  * @returns {Promise<Array>}
  */
 function runParallel(jobs, parallelNum, timeout = 1000) {
-    // асинхронная магия
+    return runWithTimeout(_runParallel(jobs, parallelNum), timeout);
+}
+
+function _runParallel(jobs, parallelNum) {
+    return new Promise(resolve => {
+        let results = [];
+        let index = 0;
+
+        function runNext() {
+            const current = index++;
+
+            if (current >= jobs.length) {
+                return resolve(results);
+            }
+
+            jobs[current]()
+                .then(r => {
+                    results[current] = r;
+                    runNext();
+                });
+        }
+
+        for (let i = 0; i < Math.min(parallelNum, jobs.length); ++i) {
+            runNext();
+        }
+    });
+}
+
+function runWithTimeout(job, timeout) {
+    return Promise.race([job, timeoutPromise(timeout)]);
+}
+
+function timeoutPromise(timeout) {
+    return new Promise(
+        (resolve, reject) => setTimeout(() => reject(new Error('Promise timeout')), timeout));
 }
 
 module.exports = {
