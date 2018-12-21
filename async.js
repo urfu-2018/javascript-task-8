6fa4450639d75cfd201d4a863dcce29f263b8518
Orilow <ilyaorlov1998@gmail.com>
2018-12-21 16:26:45 +0500

    working base tests

--- async.js
+++ async.js
@@ -13,7 +13,50 @@ const isStar = true;
  * @returns {Promise<Array>}
  */
 function runParallel(jobs, parallelNum, timeout = 1000) {
-    // асинхронная магия
+    if (!jobs.length) {
+        return Promise.resolve([]);
+    }
+
+    let jobsLeft = jobs.length;
+    const gapLength = Math.min(parallelNum, jobs.length);
+    let resultArray = [];
+    let nextJobNum = 0;
+
+    return new Promise(resolve => {
+        for (nextJobNum; nextJobNum < gapLength; nextJobNum++) {
+            doJobAsync(nextJobNum);
+        }
+
+        function doJobAsync(jobNum) {
+            function saveData(data) {
+                resultArray[jobNum] = data;
+                if (--jobsLeft === 0) {
+                    resolve(resultArray);
+
+                    return;
+                }
+
+                if (nextJobNum < jobs.length) {
+                    doJobAsync(nextJobNum++);
+                }
+            }
+
+            getJobPromiseWithTimeout(jobNum)
+                .then(result => saveData(result),
+                    error => saveData(error));
+        }
+
+        function getJobPromiseWithTimeout(jobNum) {
+            let timeoutPromise = new Promise((res, rej) =>
+                setTimeout(() => rej(new Error('Promise timeout')),
+                    timeout));
+
+            return Promise
+                .race([jobs[jobNum](), timeoutPromise]);
+        }
+
+    });
+
 }
 
 module.exports = {
