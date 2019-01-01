8a86b9ff5adfe819ea92fa815dfbde7a3f5dfffc
Bolkonyashka <33488476+Bolkonyashka@users.noreply.github.com>
2019-01-01 20:52:31 +0500

    ну такое
    У меня аннулировали эту задачку, хотя я не копипастил её, а просто использовал крутое решение, которое мне очень понравилось :) Вот извращенная версия, чтобы закрыть задачку и получить допуск

--- async.js
+++ async.js
@@ -4,7 +4,7 @@
  * Сделано задание на звездочку
  * Реализована остановка промиса по таймауту
  */
-const isStar = true;
+const isStar = false;
 
 /** Функция паралелльно запускает указанное число промисов
  * @param {Function<Promise>[]} jobs – функции, которые возвращают промисы
@@ -12,39 +12,33 @@ const isStar = true;
  * @param {Number} timeout - таймаут работы промиса
  * @returns {Promise<Array>}
  */
-function runParallel(jobs, parallelNum, timeout = 1000) {
+function runParallel(jobs, parallelNum) {
     return new Promise(resolve => {
+        let results = [];
+        let jobNumber = 0;
+
         if (jobs.length === 0) {
             resolve([]);
         }
-
-        let results = [];
-        let jobNumber = 0;
 
         for (let i = 0; i < Math.min(jobs.length, parallelNum); i++) {
             runNextJob(jobNumber++);
         }
 
         function runNextJob(jobNum) {
+            jobs[jobNum]().then(result => handleResult(result, jobNum));
+        }
 
-            function handleResult(res) {
-                results[jobNum] = res;
+        function handleResult(res, jobNum) {
+            results[jobNum] = res;
 
-                if (results.length === jobs.length) {
-                    return resolve(results);
-                }
-
-                if (jobNumber < jobs.length) {
-                    runNextJob(jobNumber++);
-                }
+            if (results.length === jobs.length) {
+                return resolve(results);
             }
 
-            Promise.race([
-                jobs[jobNum](),
-                new Promise((_, reject) => {
-                    setTimeout(reject, timeout, new Error('Promise timeout'));
-                })
-            ]).then(handleResult, handleResult);
+            if (jobNumber < jobs.length) {
+                runNextJob(jobNumber++);
+            }
         }
     });
 }
