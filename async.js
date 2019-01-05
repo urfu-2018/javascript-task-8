18c2ebc7050eb56006699b664436ec7739e0f7b5
Akishkina Veronika <nika_akishkina@mail.ru>
2019-01-05 19:20:49 +0500

    первая попытка

--- async.js
+++ async.js
@@ -6,7 +6,7 @@
  */
 const isStar = true;
 
-/** Функция паралелльно запускает указанное число промисов
+/** Функция параллельно запускает указанное число промисов
  * @param {Function<Promise>[]} jobs – функции, которые возвращают промисы
  * @param {Number} parallelNum - число одновременно исполняющихся промисов
  * @param {Number} timeout - таймаут работы промиса
@@ -14,6 +14,37 @@ const isStar = true;
  */
 function runParallel(jobs, parallelNum, timeout = 1000) {
     // асинхронная магия
+    const result = [];
+
+    return new Promise(resolve => {
+        if (!jobs[0]) {
+            resolve(result);
+        }
+        let counter = 0;
+        while (counter < parallelNum && counter < jobs.length) {
+            perform(counter++);
+        }
+
+        function perform(index) {
+            const race1 = jobs[index]();
+            const race2 = new Promise(reject => {
+                setTimeout(reject, timeout, new Error('Promise timeout'));
+            });
+
+            Promise.race([race1, race2])
+                .then(res => goNextOrFinish(res, index))
+                .catch(err => goNextOrFinish(err, index));
+        }
+
+        function goNextOrFinish(element, index) {
+            result[index] = element;
+            if (result.length === jobs.length) {
+                resolve(result);
+            } else {
+                perform(counter++);
+            }
+        }
+    });
 }
 
 module.exports = {
